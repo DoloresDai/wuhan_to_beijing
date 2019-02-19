98f309ffff77ec1c084b8ebc6af31ed195b31394
DoloresDai <841856753@qq.com>
2019-02-20 06:26:36 +0800

    Create a bus class that rewrites the price method and inherits the traffic class.

---
+++ src/main/java/company/model/Bus.java
@@ -0,0 +1,10 @@
+package main.java.company.model;
+
+public class Bus extends Vehicle {
+    public Bus (String name){
+        super("大巴车");
+    }
+    public int getPrice(){
+        return 300;
+    }
+}
